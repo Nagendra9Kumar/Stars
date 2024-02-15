ffcf9c0dbeb540877c88d1b19f0a96ad26a7470d
nagendra <nagendragubbala999@gmail.com>
2024-02-15 21:14:16 +0530

    add i❤️BVC and some mods to D

--- StarD.java
+++ StarD.java
@@ -2,7 +2,7 @@ public class StarD {
     public static void main(String args[]) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
-                if ((j == 1) || (i == 1 && j < 4) || (i == 3 && j == 5) || (i == 5 && j < 4) || (i == 4 && j == 5)
+                if ((j == 1) || (i == 1 && j <= 4) || (i == 3 && j == 5) || (i == 5 && j <= 4) || (i == 4 && j == 5)
                         || (i == 2 && j == 5)) {
                     System.out.print("* ");
                 } else
